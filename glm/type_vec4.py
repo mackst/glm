be838576465fdb94377ef3c2d92f885a56015812
Mack Stone <schistone@gmail.com>
2014-07-23 09:18:35 +0800

    implement __init__

--- glm/type_vec4.py
+++ glm/type_vec4.py
@@ -26,7 +26,86 @@
 class Vec4(object):
 
     def __init__(self, *args, **kwargs):
-        pass
+        if kwargs:
+            self.x = kwargs.get('x', .0)
+            self.y = kwargs.get('y', .0)
+            self.z = kwargs.get('z', .0)
+            self.w = kwargs.get('w', .0)
+        elif args:
+            lenArgs = len(args)
+            if lenArgs == 1:
+                inarg = args[0]
+                if isinstance(inarg, Vec4):
+                    self.x = inarg.x
+                    self.y = inarg.y
+                    self.z = inarg.z
+                    self.w = inarg.w
+                # TODO: implement vec3 and vec2
+                #if isinstance(inarg, Vec3):
+                elif isinstance(inarg, list) or isinstance(inarg, tuple):
+                    il = []
+                    if len(inarg) == 1:
+                        il = inarg[0] + [.0, .0, .0]
+                    elif len(inarg) == 2:
+                        il = inarg[0:] + [.0, .0]
+                    elif len(inarg) == 3:
+                        il = inarg[0:] + [.0,]
+                    elif len(inarg) == 4:
+                        il = inarg
+                    else:
+                        il = inarg[:4]
+                    self.x, self.y, self.z, self.w = il
+                elif isinstance(inarg, int) or isinstance(inarg, float) or isinstance(inarg, long):
+                    self.x = inarg
+                    self.y = inarg
+                    self.z = inarg
+                    self.w = inarg
+            elif lenArgs == 2:
+                self.x, self.y = args
+                self.z = .0
+                self.w = .0
+            elif lenArgs == 3:
+                self.x, self.y, self.z = args
+                self.w = .0
+            elif lenArgs == 4:
+                self.x, self.y, self.z, self.w = args
+            else:
+                self.x, self.y, self.z, self.w = args[:4]
+        else:
+            self.x = .0
+            self.y = .0
+            self.z = .0
+            self.w = .0
 
     def __len__(self):
         return 4
+
+    def __getitem__(self, index):
+        if index > 3 or index < -4:
+            raise IndexError('out of range')
+
+        if index == 0 or index == -4:
+            return self.x
+        elif index == 1 or index == -3:
+            return self.y
+        elif index == 2 or index == -2:
+            return self.z
+        elif index == 3 or index == -1:
+            return self.w
+        return super(Vec4, self).__getitem__(index)
+
+    def __setitem__(self, index, value):
+        if index > 3 or index < -4:
+            raise IndexError('out of range')
+
+        if index == 0 or index == -4:
+            self.x = value
+        elif index == 1 or index == -3:
+            self.y = value
+        elif index == 2 or index == -2:
+            self.z = value
+        elif index == 3 or index == -1:
+            self.w = value
+
+        return super(Vec4, self).__setitem__(index, value)
+
