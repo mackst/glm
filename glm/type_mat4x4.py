90861bc03a936fc0132cf41aba481ca86cdbcc35
Mack Stone <schistone@gmail.com>
2014-07-24 10:06:41 +0800

    implement __init__

--- glm/type_mat4x4.py
+++ glm/type_mat4x4.py
@@ -27,8 +27,82 @@ from type_vec4 import Vec4
 class Mat4x4(object):
 
     def __init__(self, *args, **kwargs):
+        v0 = Vec4(x=1.0)
+        v1 = Vec4(y=1.0)
+        v2 = Vec4(z=1.0)
+        v3 = Vec4(w=1.0)
+        if kwargs:
+            v0.x = kwargs.get('x0', 0.0)
+            v0.y = kwargs.get('y0', 0.0)
+            v0.z = kwargs.get('z0', 0.0)
+            v0.w = kwargs.get('w0', 0.0)
+            v1.x = kwargs.get('x1', 0.0)
+            v1.y = kwargs.get('y1', 0.0)
+            v1.z = kwargs.get('z1', 0.0)
+            v1.w = kwargs.get('w1', 0.0)
+            v2.x = kwargs.get('x2', 0.0)
+            v2.y = kwargs.get('y2', 0.0)
+            v2.z = kwargs.get('z2', 0.0)
+            v2.w = kwargs.get('w2', 0.0)
+            v3.x = kwargs.get('x3', 0.0)
+            v3.y = kwargs.get('y3', 0.0)
+            v3.z = kwargs.get('z3', 0.0)
+            v3.w = kwargs.get('w3', 0.0)
+        elif args:
+            lenArgs = len(args)
+            if lenArgs == 1:
+                m = args[0]
+                if isinstance(m, Mat4x4):
+                    v0 = m[0]
+                    v1 = m[1]
+                    v2 = m[2]
+                    v3 = m[3]
+                elif isinstance(m, list) or isinstance(m, tuple):
+                    if len(m) != 16:
+                        raise TypeError('list or tuple must have 16 items')
+                    v0.x = m[0]
+                    v1.x = m[1]
+                    v2.x = m[2]
+                    v3.x = m[3]
+                    v0.y = m[4]
+                    v1.y = m[5]
+                    v2.y = m[6]
+                    v3.y = m[7]
+                    v0.z = m[8]
+                    v1.z = m[9]
+                    v2.z = m[10]
+                    v3.z = m[11]
+                    v0.w = m[12]
+                    v1.w = m[13]
+                    v2.w = m[14]
+                    v3.w = m[15]
+                elif isinstance(m, float) or isinstance(m, int) or isinstance(m, long):
+                    v0 = Vec4(x=m)
+                    v1 = Vec4(y=m)
+                    v2 = Vec4(z=m)
+                    v3 = Vec4(w=m)
+            elif lenArgs == 4:
+                if isinstance(args[0], Vec4):
+                    v0 = args[0]
+                elif isinstance(args[0], list) or isinstance(args[0], tuple):
+                    v0 = Vec4(args[0])
 
-        self.__value = ()
+                if isinstance(args[1], Vec4):
+                    v1 = args[1]
+                elif isinstance(args[1], list) or isinstance(args[1], tuple):
+                    v1 = Vec4(args[1])
+
+                if isinstance(args[2], Vec4):
+                    v2 = args[2]
+                elif isinstance(args[2], list) or isinstance(args[2], tuple):
+                    v2 = Vec4(args[2])
+
+                if isinstance(args[3], Vec4):
+                    v3 = args[3]
+                elif isinstance(args[3], list) or isinstance(args[3], tuple):
+                    v3 = Vec4(args[3])
+
+        self.__value = (v0, v1, v2, v3)
 
     def __len__(self):
         return 4
