ca5e41fb75c889a502b1a9ca878ece7922611d7f
Mack stone <schistone@gmail.com>
2018-04-27 21:13:36 +0800

    fix find_packages

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests'], include=['glm']),  # Required
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'], where='glm'),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
